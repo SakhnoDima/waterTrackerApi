0165d3e3c625a6c9d94540716036d56a762fd9b2
SakhnoDima <134371251+SakhnoDima@users.noreply.github.com>
2023-12-01 11:50:56 +0200

    initial
    added mongoDb, schemas
    controller register

--- constant/constant.js
+++ constant/constant.js
@@ -1,5 +1,7 @@
 const emailRegex = /[a-z0-9]+@+[a-z]+[\./]+[a-z]{2,3}/;
+
 const gender = { male: "male", female: "female" };
+
 const httpErrors = {
   400: "Bad Request",
   401: "Not authorized",
