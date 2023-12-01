a9dcb8883b407e0ccb40be51a64644f78b5f3ad7
SakhnoDima <134371251+SakhnoDima@users.noreply.github.com>
2023-12-01 17:37:03 +0200

    route userUpdate
    added userUpdate route,
    added userUpdate schema

--- controllers/user.js
+++ controllers/user.js
@@ -17,6 +17,41 @@ const getCurrent = async (req, res) => {
   });
 };
 
+//? ===  Update User Info ===
+
+const userUpdate = async (req, res) => {
+  const { _id, email } = req.user;
+  const { email: newEmail, password } = req.body;
+
+  // is user with new email exist in base
+
+  if (newEmail && email !== newEmail) {
+    const user = await User.findOne({ email: newEmail });
+    if (user) {
+      throw HttpError(401, `User with email: ${newEmail} already exist`);
+    }
+  }
+
+  // update user
+
+  const newUser = await User.findByIdAndUpdate(_id, req.body, {
+    new: true,
+  });
+  if (password) {
+    newUser.setPass(password);
+    await newUser.save();
+  }
+
+  res.status(200).json({
+    user: {
+      name: newUser.name,
+      email: newUser.email,
+      gender: newUser.gender,
+    },
+  });
+};
+
 module.exports = {
   getCurrent: controllerWrapper(getCurrent),
+  userUpdate: controllerWrapper(userUpdate),
 };
--- routes/user.js
+++ routes/user.js
@@ -1,12 +1,14 @@
 const express = require("express");
 const router = express.Router();
 
-const { getCurrent } = require("../controllers/user");
-
+const { getCurrent, userUpdate } = require("../controllers/user");
+const { validateBody } = require("../middlewares/index.js");
+const { schemas } = require("../models/user.js");
 const authenticate = require("../middlewares/isAuthenticate");
 
 router.use(authenticate);
 
 router.get("/current", getCurrent);
+router.patch("/updateUser", validateBody(schemas.userUpdate), userUpdate);
 
 module.exports = router;
