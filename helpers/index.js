49e13281fa8b3672590a4341bc0976235c684156
SakhnoDima <134371251+SakhnoDima@users.noreply.github.com>
2023-12-01 14:46:57 +0200

    added logIn rout
    rout log in
    helpers: tokenWorkPlace
    middleware: isAuth
    
    change password settings from helpers to User methods

--- helpers/index.js
+++ helpers/index.js
@@ -1,9 +1,12 @@
 const handleMongooseError = require("./handleMongooseError");
 const HttpError = require("./httpErrors");
 const controllerWrapper = require("./controllerWrapper");
+const { tokenCreator, isValidToken } = require("./tokenWorkPlace");
 
 module.exports = {
   handleMongooseError,
   HttpError,
   controllerWrapper,
+  tokenCreator,
+  isValidToken,
 };
--- routes/auth.js
+++ routes/auth.js
@@ -1,10 +1,11 @@
 const express = require("express");
 const router = express.Router();
 
-const { register } = require("../controllers/auth");
+const { register, logIn } = require("../controllers/auth");
 const { validateBody } = require("../middlewares/index.js");
 const { schemas } = require("../models/user.js");
 
 router.post("/register", validateBody(schemas.userJoiSchema), register);
+router.post("/login", validateBody(schemas.userJoiSchema), logIn);
 
 module.exports = router;
