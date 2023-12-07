d19b0a37190dafb1d1f12ed230fb1076f949d176
SakhnoDima <134371251+SakhnoDima@users.noreply.github.com>
2023-12-07 19:29:38 +0200

    email verify
    Send email to user id change email

--- routes/user.js
+++ routes/user.js
@@ -6,16 +6,20 @@ const {
   userUpdate,
   uploadAvatar,
   editWaterRate,
+  verifyEmail,
 } = require("../controllers/user");
 
 const { validateBody, upload } = require("../middlewares/index.js");
 const { schemas } = require("../models/user.js");
 const authenticate = require("../middlewares/isAuthenticate");
 
+router.get("/updateUser/:email/:id", verifyEmail);
+
 router.use(authenticate);
 
 router.get("/current", getCurrent);
 router.patch("/updateUser", validateBody(schemas.userUpdate), userUpdate);
+
 router.patch("/updateAvatar", upload.single("file"), uploadAvatar);
 router.patch(
   "/updateWaterRate",
